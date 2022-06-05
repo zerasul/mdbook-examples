4823f35c0fdec553def09ff7369fa9155df029da
zerasul <zerasul@gmail.com>
2022-06-05 19:22:26 +0200

    example 3

---
+++ ej3.controls2/src/main.c
@@ -0,0 +1,103 @@
+/**
+ * Example 3: Asynchronous Controls
+ **/
+#include <genesis.h>
+#include "constants.h"
+
+void inputHandler(u16,u16,u16);
+void printChar(char*, u16, u16);
+
+int main()
+{
+    JOY_init();
+
+    JOY_setEventHandler(inputHandler);
+
+    while(1)
+    {
+        //For versions prior to SGDK 1.60 use VDP_waitVSync instead.
+        SYS_doVBlankProcess();
+    }
+    return (0);
+}
+
+void printChar(char* text, u16 tilex, u16 tiley){
+    VDP_drawText(text,tilex,tiley);
+}
+
+void inputHandler(u16 joy,u16 changed,u16 state){
+
+
+        if (changed & state & BUTTON_START)
+		{
+			printChar(START_TEXT, POSX_START, POSY_RIGHT);
+		}
+		else 
+		{
+			printChar(EMPTY_TEXT, POSX_START, POSY_RIGHT);	
+		}
+
+        if (changed & state & BUTTON_UP)
+		{
+			printChar(UP_TEXT, POSX_UP, POSY_UP);
+		}
+		else 
+		{
+			printChar(EMPTY_TEXT, POSX_UP, POSY_UP);	
+		}
+
+        if (changed & state & BUTTON_LEFT)
+		{
+			printChar(LEFT_TEXT, POSX_LEFT, POSY_LEFT);
+		}
+		else 
+		{
+			printChar(EMPTY_TEXT, POSX_LEFT, POSY_LEFT);	
+		}
+
+        if (changed & state & BUTTON_DOWN)
+		{
+			printChar(DOWN_TEXT, POSX_DOWN, POSY_DOWN);
+		}
+		else 
+		{
+			printChar(EMPTY_TEXT, POSX_DOWN, POSY_DOWN);	
+		}
+
+        if (changed & state & BUTTON_RIGHT)
+		{
+			printChar(RIGTH_TEXT, POSX_RIGHT, POSY_RIGHT);
+		}
+		else 
+		{
+			printChar(EMPTY_TEXT, POSX_RIGHT, POSY_RIGHT);	
+		}
+
+
+        if (changed & state & BUTTON_A)
+		{
+			printChar(A_TEXT, POSX_A, POSY_RIGHT);
+		}
+		else 
+		{
+			printChar(EMPTY_TEXT, POSX_A, POSY_RIGHT);	
+		}
+
+        if (changed & state & BUTTON_B)
+		{
+			printChar(B_TEXT, POSX_B, POSY_RIGHT);
+		}
+		else 
+		{
+			printChar(EMPTY_TEXT, POSX_B, POSY_RIGHT);	
+		}
+
+        if (changed & state & BUTTON_C)
+		{
+			printChar(C_TEXT, POSX_C, POSY_RIGHT);
+		}
+		else 
+		{
+			printChar(EMPTY_TEXT, POSX_C, POSY_RIGHT);	
+		}
+}
